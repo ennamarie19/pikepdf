4e8d87b6071c7417a57ee44db2d7f0886549bc3a
bcapuano <bcapuano@asu.edu>
2024-12-23 19:20:14 -0700

    incremental test

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -1,20 +1,17 @@
 # SPDX-FileCopyrightText: 2024 ennamarie19
 # SPDX-License-Identifier: MIT
 export QPDF_SOURCE_TREE="$SRC"/qpdf
-export QPDF_BUILD_LIBDIR=$QPDF_SOURCE_TREE/build/libqpdf
 export QPDF_BUILD_LIBDIR=$OUT/src/qpdf/build/libqpdf
 
 # Build qpdf dependency
-
 cd $QPDF_SOURCE_TREE
 ./fuzz/oss-fuzz-build
 
 # Build pikepdf
 cd "$SRC"/pikepdf
-env QPDF_SOURCE_TREE=$QPDF_SOURCE_TREE QPDF_BUILD_LIBDIR=$QPDF_BUILD_LIBDIR \
+env LD_LIBRARY_PATH=$QPDF_BUILD_LIBDIR QPDF_SOURCE_TREE=$QPDF_SOURCE_TREE QPDF_BUILD_LIBDIR=$QPDF_BUILD_LIBDIR \
     CC="$CC" CFLAGS="$CFLAGS" CXX="$CXX" CXXFLAGS="$CXXFLAGS" LDSHARED="$CXX -shared" \
     pip3 install --verbose .
-
 
 # Build fuzzers in $OUT
 for fuzzer in $(find fuzzing -name '*_fuzzer.py');do
